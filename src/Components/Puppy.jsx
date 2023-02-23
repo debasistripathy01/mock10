3e38975945796f108a3379fc9534e863f9a1b752
debasistripathy01 <debasistripathy04@gmail.com>
2023-02-23 14:18:39 +0530

    mock10

--- src/Components/Puppy.jsx
+++ src/Components/Puppy.jsx
@@ -1,8 +1,52 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import css from "./home.module.css"
 
 const Puppy = () => {
+    // const { breed_name } = useParams();
+  const [images, setImages] = useState([]);
+
+  const location = useLocation()
+  let breed_name = location.pathname.split("/")[2]
+
+
+  const handleFetch=(breed_name)=>{
+
+    axios.get(`https://dog.ceo/api/breed/${breed_name}/images`)
+      .then(response => {
+        setImages(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+}
+
+
+  useEffect(() => {
+    handleFetch(breed_name)
+  }, [breed_name]);
+
+console.log(images)
+// useEffect(()=>{
+//     handleFetch(breed_name);
+// }, [])
+  console.log("breed : ",breed_name)
+
   return (
-    <div>Puppy</div>
+    <Box className={css.puppy}>
+        {images.map((el)=>(
+            
+            <div className={css.image}>
+                
+                    <Box >
+                        <img src={el} alt={breed_name}/>
+                    </Box>
+              </div>
+            ))}
+    </Box>
   )
 }
 
